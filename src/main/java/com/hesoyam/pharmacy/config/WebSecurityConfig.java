e7a69219282ee97d2f6044ca4d69bed0f4b3499d
Nikola Dragic <nikola_dragic@hotmail.com>
2021-01-15 22:14:35 +0100

    feat: Spring Boot project initialized

---
+++ src/main/java/com/hesoyam/pharmacy/config/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.hesoyam.pharmacy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("*").permitAll().and().cors();
+    }
+}
