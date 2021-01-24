2c3dc7a9193f851b371062bfb7b08a0b0fe5636d
Nikola Dragic <nikola_dragic@hotmail.com>
2021-01-24 14:24:57 +0100

    feat: Phone number constraint + validator implemented

---
+++ src/main/java/com/hesoyam/pharmacy/user/validators/PhoneNumberConstraint.java
@@ -0,0 +1,15 @@
+package com.hesoyam.pharmacy.user.validators;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PhoneNumberValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PhoneNumberConstraint {
+    String message() default "Invalid phone number";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
