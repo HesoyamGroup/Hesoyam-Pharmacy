48aeb51878bfe5f2fbbba2f2f8cd2b90723022b2
Nikola Dragic <nikola_dragic@hotmail.com>
2021-01-25 20:24:15 +0100

    chore:[dev] Allow CORS using ENV variable

--- src/main/java/com/hesoyam/pharmacy/config/WebConfig.java
+++ src/main/java/com/hesoyam/pharmacy/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.hesoyam.pharmacy.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,9 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${frontend.url}")
+    private String frontEndUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+        registry.addMapping("/**").allowedOrigins(frontEndUrl);
     }
 }
