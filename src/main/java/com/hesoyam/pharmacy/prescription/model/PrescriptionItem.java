a351a15ba57c72397bb99aec72b7b92af47a72ce
Nikola Dragic <nikola_dragic@hotmail.com>
2021-01-26 19:18:40 +0100

    feat: Prescription model validation

--- src/main/java/com/hesoyam/pharmacy/prescription/model/PrescriptionItem.java
+++ src/main/java/com/hesoyam/pharmacy/prescription/model/PrescriptionItem.java
@@ -8,6 +8,7 @@ package com.hesoyam.pharmacy.prescription.model;
 import com.hesoyam.pharmacy.medicine.model.Medicine;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 
 @Entity
 public class PrescriptionItem {
@@ -16,10 +17,37 @@ public class PrescriptionItem {
    private Long id;
 
    @Column
+   @Min(value = 0, message = "Quantity must be non-negative integer.")
    private int quantity;
 
    @ManyToOne(fetch = FetchType.EAGER, optional=false)
    @JoinColumn(name="medicine_id")
    private Medicine medicine;
 
+   public PrescriptionItem(){}
+
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public int getQuantity() {
+      return quantity;
+   }
+
+   public void setQuantity(int quantity) {
+      this.quantity = quantity;
+   }
+
+   public Medicine getMedicine() {
+      return medicine;
+   }
+
+   public void setMedicine(Medicine medicine) {
+      this.medicine = medicine;
+   }
 }
