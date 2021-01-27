2d9f5de473c6508536bb3920264b0fa883886c6e
Nikola Dragic <nikola_dragic@hotmail.com>
2021-01-27 17:09:20 +0100

    feat: User role enum added

---
+++ src/main/java/com/hesoyam/pharmacy/user/model/RoleEnum.java
@@ -0,0 +1,12 @@
+package com.hesoyam.pharmacy.user.model;
+
+public enum RoleEnum {
+    NONE,
+    PATIENT,
+    PHARMACIST,
+    DERMATOLOGIST,
+    ADMINISTRATOR,
+    SYS_ADMIN,
+    SUPPLIER;
+
+}
