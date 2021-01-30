0670d4784eba843b38f5c708b66f9e06cfafb4d0
vucaj <vucaj1998@outlook.com>
2021-01-30 21:46:59 +0100

    feat: InventoryItemReservationDTO

---
+++ src/main/java/com/hesoyam/pharmacy/pharmacy/DTO/InventoryItemReservationDTO.java
@@ -0,0 +1,36 @@
+package com.hesoyam.pharmacy.pharmacy.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class InventoryItemReservationDTO {
+
+    @NotNull(message = "Pharmacy ID must be provided")
+    private Long pharmacyId;
+
+    @NotNull(message = "Medicine ID must be provided")
+    private Long medicineId;
+
+    public InventoryItemReservationDTO() {
+    }
+
+    public InventoryItemReservationDTO(@NotNull(message = "Pharmacy ID must be provided") Long pharmacyId, @NotNull(message = "Medicine ID must be provided") Long medicineId) {
+        this.pharmacyId = pharmacyId;
+        this.medicineId = medicineId;
+    }
+
+    public Long getPharmacyId() {
+        return pharmacyId;
+    }
+
+    public void setPharmacyId(Long pharmacyId) {
+        this.pharmacyId = pharmacyId;
+    }
+
+    public Long getMedicineId() {
+        return medicineId;
+    }
+
+    public void setMedicineId(Long medicineId) {
+        this.medicineId = medicineId;
+    }
+}
