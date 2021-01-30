ea6e70ddcca8296b2ab12d5bd893651a7e39908d
vucaj <vucaj1998@outlook.com>
2021-01-30 22:50:33 +0100

    feat: medicine reservation fully working

--- src/main/java/com/hesoyam/pharmacy/pharmacy/service/impl/InventoryService.java
+++ src/main/java/com/hesoyam/pharmacy/pharmacy/service/impl/InventoryService.java
@@ -4,10 +4,12 @@ import com.hesoyam.pharmacy.pharmacy.model.Inventory;
 import com.hesoyam.pharmacy.pharmacy.repository.InventoryRepository;
 import com.hesoyam.pharmacy.pharmacy.service.IInventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
+@Service
 public class InventoryService implements IInventoryService {
 
     @Autowired
