b7a799e9dd69e65534961055427e400f947519aa
Nikola Dragic <nikola_dragic@hotmail.com>
2021-02-01 17:32:01 +0100

    feat: Update getPharmacyByMedicine availability query

--- src/main/java/com/hesoyam/pharmacy/pharmacy/repository/PharmacyRepository.java
+++ src/main/java/com/hesoyam/pharmacy/pharmacy/repository/PharmacyRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
 
-    @Query("select p from Pharmacy p join p.inventory i join i.inventoryItems it join it.medicine m where m.id = :medicine_id and it.available-it.reserved>0")
+    @Query("select p from Pharmacy p join p.inventory i join i.inventoryItems it join it.medicine m where m.id = :medicine_id and it.available>0")
     List<Pharmacy> getPharmacyByMedicineAvailability(@Param("medicine_id") Long medicineId);
 }
