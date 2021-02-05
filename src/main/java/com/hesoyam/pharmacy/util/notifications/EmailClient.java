dd9fe14b07e1da386a95b028ea19e4203aca2045
Gergo <49314980+gregVader@users.noreply.github.com>
2021-02-05 12:11:51 +0100

    feat: Sending mail to subscribed patients on new promotion

--- src/main/java/com/hesoyam/pharmacy/util/notifications/EmailClient.java
+++ src/main/java/com/hesoyam/pharmacy/util/notifications/EmailClient.java
@@ -5,17 +5,27 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 @Component
 public class EmailClient {
     @Autowired
     private JavaMailSender mailSender;
 
+    private static final int NUM_OF_QUICK_SERVICE_THREADS = 10;
+
+    private final ScheduledExecutorService quickService = Executors.newScheduledThreadPool(NUM_OF_QUICK_SERVICE_THREADS);
+
     public void sendEmail(EmailObject emailObject){
         SimpleMailMessage email = new SimpleMailMessage();
-        email.setTo(emailObject.getRecipient());
+        email.setTo(emailObject.getRecipients().toArray(String[]::new));
         email.setSubject(emailObject.getSubject());
         email.setText(emailObject.getBody());
-        mailSender.send(email);
+
+        quickService.submit(() -> mailSender.send(email));
     }
 
 }
