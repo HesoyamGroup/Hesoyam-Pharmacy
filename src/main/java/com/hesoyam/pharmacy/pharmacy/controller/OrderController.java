987bfda3202d426bd2c12b6b783a48fb091a8395
Gergo <49314980+gregVader@users.noreply.github.com>
2021-02-06 20:54:06 +0100

    feat: Create orders

--- src/main/java/com/hesoyam/pharmacy/pharmacy/controller/OrderController.java
+++ src/main/java/com/hesoyam/pharmacy/pharmacy/controller/OrderController.java
@@ -1,12 +1,16 @@
 package com.hesoyam.pharmacy.pharmacy.controller;
 
 import com.hesoyam.pharmacy.pharmacy.dto.ShowOrdersDTO;
+import com.hesoyam.pharmacy.pharmacy.model.Order;
 import com.hesoyam.pharmacy.pharmacy.service.IOrderService;
+import com.hesoyam.pharmacy.user.model.Administrator;
 import com.hesoyam.pharmacy.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,5 +39,18 @@ public class OrderController {
         }catch (EntityNotFoundException e){
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PreAuthorize("hasRole('ADMINISTRATOR')")
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ShowOrdersDTO> create(@AuthenticationPrincipal Administrator administrator, @RequestBody ShowOrdersDTO order){
+        try{
+            Order newOrder = orderService.create(order, administrator);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ShowOrdersDTO(newOrder));
+        } catch(EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+
+
     }
 }
--- src/main/java/com/hesoyam/pharmacy/pharmacy/dto/ShowOrdersDTO.java
+++ src/main/java/com/hesoyam/pharmacy/pharmacy/dto/ShowOrdersDTO.java
@@ -1,6 +1,9 @@
 package com.hesoyam.pharmacy.pharmacy.dto;
 
+import com.hesoyam.pharmacy.pharmacy.model.Order;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShowOrdersDTO {
@@ -10,7 +13,15 @@ public class ShowOrdersDTO {
     private List<ShowOrderItemDTO> showOrderItemDTOS;
 
     public ShowOrdersDTO(){
+        //Empty ctor for JSON serializer
+    }
 
+    public ShowOrdersDTO(Order order){
+        this.id = order.getId();
+        this.deadLine = order.getDeadLine();
+        this.pharmacyName = order.getPharmacy().getName();
+        this.showOrderItemDTOS = new ArrayList<>();
+        order.getOrderItems().forEach(item -> showOrderItemDTOS.add(new ShowOrderItemDTO(item)));
     }
 
     public ShowOrdersDTO(Long id, LocalDateTime deadLine, String pharmacyName, List<ShowOrderItemDTO> showOrderItemDTOS) {
@@ -45,6 +56,8 @@ public class ShowOrdersDTO {
     }
 
     public List<ShowOrderItemDTO> getShowOrderItemDTOS() {
+        if(showOrderItemDTOS == null)
+            return new ArrayList<>();
         return showOrderItemDTOS;
     }
 
