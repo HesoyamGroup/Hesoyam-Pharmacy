0d792014de58befc2e4a010756a0a05551d52dd2
Gergo <49314980+gregVader@users.noreply.github.com>
2021-02-09 16:29:24 +0100

    fix: Frontend URL environment variable for CORS policy

--- src/main/java/com/hesoyam/pharmacy/config/WebSecurityConfig.java
+++ src/main/java/com/hesoyam/pharmacy/config/WebSecurityConfig.java
@@ -48,9 +48,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .cors().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/auth/**").permitAll().and()
-                .cors().and().csrf().disable()
+                .csrf().disable()
                 .addFilterBefore(new TokenAuthenticationFilter(tokenUtils, userService),
                         BasicAuthenticationFilter.class);
 
